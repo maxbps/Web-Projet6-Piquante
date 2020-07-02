4c635c34ddd0d5567971eb935a584bc1581db7d2
maxbps <bps_maxime@outlook.fr>
2020-07-02 08:32:34 +0200

    i changed files for sauces ...

--- back-end/controllers/stuff.js
+++ back-end/controllers/stuff.js
@@ -1,49 +1,113 @@
-const Thing = require('../models/things');
+const Sauce = require('../models/sauce')
 const fs = require('fs')
 
-exports.createThing = (req, res, next) => {
-    const thingObject = JSON.parse(req.body.thing);
-    delete thingObject._id;
-    const thing = new Thing({
-        ...thingObject,
-        imageUrl: `${req.protocol}://${req.get('host')}/images/${req.file.filename}`
-    });
-    thing.save()
-        .then(() => res.status(201).json({ message: 'Objet enregistré !' }))
-        .catch(error => res.status(400).json({ error }));
-};
 
-exports.getOneThing = (req, res, next) => {
-    Thing.findOne({ _id: req.params.id })
-        .then(thing => res.status(200).json(thing))
+exports.getAllSauces = (req, res, next) => {
+    Sauce.find()
+        .then(sauces => res.status(200).json(sauces))
+        .catch(error => res.status(400).json({ error }))
+}
+
+exports.getOneSauce = (req, res, next) => {
+    Sauce.findOne({ _id: req.params.id })
+        .then(sauce => res.status(200).json(sauce))
         .catch(error => res.status(404).json({ error }))
 }
 
-exports.modifyThing = (req, res, next) => {
-    const thingObject = req.file ? {
-        ...JSON.parse(req.body.thing),
+exports.createSauce = (req, res, next) => {
+    const sauceObject = JSON.parse(req.body.sauce)
+    delete sauceObject._id
+    sauceObject.likes = 0
+    sauceObject.dislikes = 0
+    sauceObject.usersLiked = []
+    sauceObject.usersDisliked = []
+    const sauce = new Sauce({
+        ...sauceObject,
         imageUrl: `${req.protocol}://${req.get('host')}/images/${req.file.filename}`
-    } : {...req.body };
-    Thing.updateOne({ _id: req.params.id }, {...thingObject, _id: req.params.id })
-        .then(() => res.status(200).json({ message: 'Objet modifié !' }))
-        .catch(error => res.status(400).json({ error }));
-};
-
-exports.deleteThing = (req, res, next) => {
-    Thing.findOne({ _id: req.params.id })
-        .then(thing => {
-            const filename = thing.imageUrl.split('/images/')[1];
-            fs.unlink(`images/${filename}`, () => {
-                Thing.deleteOne({ _id: req.params.id })
-                    .then(() => res.status(200).json({ message: 'Objet supprimé !' }))
-                    .catch(error => res.status(400).json({ error }));
-            });
-        })
-        .catch(error => res.status(500).json({ error }));
-};
-
-exports.getAllStuff = (req, res, next) => {
-    Thing.find()
-        .then(things => res.status(200).json(things))
+    })
+    sauce.save()
+        .then(() => res.status(201).json({ message: 'Objet enregistré !' }))
         .catch(error => res.status(400).json({ error }))
 }
+
+
+exports.modifySauce = (req, res, next) => {
+    const sauceObject = req.file ? {
+        ...JSON.parse(req.body.sauce),
+        imageUrl: `${req.protocol}://${req.get('host')}/images/${req.file.filename}`
+    } : {...req.body }
+    Sauce.updateOne({ _id: req.params.id }, {...sauceObject, _id: req.params.id })
+        .then(() => res.status(200).json({ message: 'Objet modifiée !' }))
+        .catch(error => res.status(400).json({ error }))
+}
+
+exports.deleteSauce = (req, res, next) => {
+    Sauce.findOne({ _id: req.params.id })
+        .then(sauce => {
+            const filename = sauce.imageUrl.split('/images/')[1]
+            fs.unlink(`images/${filename}`,
+                () => {
+                    Sauce.deleteOne({ _id: req.params.id })
+                        .then(() => res.status(200).json({ message: 'Objet supprimée !' }))
+                        .catch(error => res.status(400).json({ error }))
+                })
+        })
+        .catch(error => res.status(500).json({ error }))
+}
+
+
+
+
+
+
+
+
+// const Thing = require('../models/things');
+// const fs = require('fs')
+
+// exports.createThing = (req, res, next) => {
+//     const thingObject = JSON.parse(req.body.thing);
+//     delete thingObject._id;
+//     const thing = new Thing({
+//         ...thingObject,
+//         imageUrl: `${req.protocol}://${req.get('host')}/images/${req.file.filename}`
+//     });
+//     thing.save()
+//         .then(() => res.status(201).json({ message: 'Objet enregistré !' }))
+//         .catch(error => res.status(400).json({ error }));
+// };
+
+// exports.getOneThing = (req, res, next) => {
+//     Thing.findOne({ _id: req.params.id })
+//         .then(thing => res.status(200).json(thing))
+//         .catch(error => res.status(404).json({ error }))
+// }
+
+// exports.modifyThing = (req, res, next) => {
+//     const thingObject = req.file ? {
+//         ...JSON.parse(req.body.thing),
+//         imageUrl: `${req.protocol}://${req.get('host')}/images/${req.file.filename}`
+//     } : {...req.body };
+//     Thing.updateOne({ _id: req.params.id }, {...thingObject, _id: req.params.id })
+//         .then(() => res.status(200).json({ message: 'Objet modifié !' }))
+//         .catch(error => res.status(400).json({ error }));
+// };
+
+// exports.deleteThing = (req, res, next) => {
+//     Thing.findOne({ _id: req.params.id })
+//         .then(thing => {
+//             const filename = thing.imageUrl.split('/images/')[1];
+//             fs.unlink(`images/${filename}`, () => {
+//                 Thing.deleteOne({ _id: req.params.id })
+//                     .then(() => res.status(200).json({ message: 'Objet supprimé !' }))
+//                     .catch(error => res.status(400).json({ error }));
+//             });
+//         })
+//         .catch(error => res.status(500).json({ error }));
+// };
+
+// exports.getAllStuff = (req, res, next) => {
+//     Thing.find()
+//         .then(things => res.status(200).json(things))
+//         .catch(error => res.status(400).json({ error }))
+// }
--- back-end/routes/stuff.js
+++ back-end/routes/stuff.js
@@ -1,13 +1,31 @@
-const express = require('express');
-const router = express.Router();
+const express = require('express')
+const router = express.Router()
 const auth = require('../middleware/auth')
-const stuffCtrl = require('../controllers/stuff');
+const stuffCtrl = require('../controllers/stuff')
 const multer = require('../middleware/multer-config')
 
-router.get('/', auth, stuffCtrl.getAllStuff);
-router.post('/', auth, multer, stuffCtrl.createThing);
-router.get('/:id', auth, stuffCtrl.getOneThing);
-router.put('/:id', auth, multer, stuffCtrl.modifyThing);
-router.delete('/:id', auth, stuffCtrl.deleteThing);
+router.get('/', stuffCtrl.getAllSauces) // display sauces, no need to be login
+router.get('/:id', auth, stuffCtrl.getOneSauce) // display a sauce
+router.post('/', auth, multer, stuffCtrl.createSauce) // create a new sauce
+router.put('/:id', auth, multer, stuffCtrl.modifySauce) // change a sauce
+router.delete('/:id', auth, stuffCtrl.deleteSauce) // delete a sauce
 
-module.exports = router;
+
+module.exports = router
+
+
+
+
+// const express = require('express');
+// const router = express.Router();
+// const auth = require('../middleware/auth')
+// const stuffCtrl = require('../controllers/stuff');
+// const multer = require('../middleware/multer-config')
+
+// router.get('/', auth, stuffCtrl.getAllStuff);
+// router.post('/', auth, multer, stuffCtrl.createThing);
+// router.get('/:id', auth, stuffCtrl.getOneThing);
+// router.put('/:id', auth, multer, stuffCtrl.modifyThing);
+// router.delete('/:id', auth, stuffCtrl.deleteThing);
+
+// module.exports = router;
